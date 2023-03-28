5166c0bbe830a0ac0fbce6ab9b1499418e5d53d5
Susuki_Yosuke <hit2250.susuki.yosuke@gmail.com>
2023-03-28 19:58:01 +0900

    リンクタグ修正

--- components/layoutCmp/layoutList/Header.js
+++ components/layoutCmp/layoutList/Header.js
@@ -5,6 +5,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css"
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link"
 
 export default function Header(props) {
   return (
@@ -14,7 +15,7 @@ export default function Header(props) {
         <a href='https://github.com/YosukeSusuki' target='brank'><FontAwesomeIcon icon={faGithub} /></a> 
         <p><FontAwesomeIcon icon={faPaperPlane} /></p>
       </div>
-      <a className="flex jusCen" href="/"><img src="/images/logo.svg" alt="logo" /></a>
+      <Link className="flex jusCen" href="/"><img src="/images/logo.svg" alt="logo" /></Link>
       <nav>{props.change()}</nav>
     </header>
   )
